02285f9a1f4e61b4f2b3237d1b6c0f58f3fc80bb
RoySamson-stack <rsam9009@gmail.com>
2021-09-30 11:42:18 +0300

    enabling the message to be seen by the user

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ let hasBlackJack = false;
 let sum = firstCard + secondCard;
 let isAlive = true;
 let message = "";
+var messageEl = document.getElementById('title_message')
 
-if(sum <= 20){
-  message = "Do you want to draw a new card?";
-}else if(sum === 21){
-  message = "Woohoo! You've got Blackjack!";
-  hasBlackJack = true
-}else{
-  message = "You're out of the game"
-  isAlive = false
+function startGame() {
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game"
+    isAlive = false
+  }
+messageEl.textContent = message
 }
-
-console.log(message)
